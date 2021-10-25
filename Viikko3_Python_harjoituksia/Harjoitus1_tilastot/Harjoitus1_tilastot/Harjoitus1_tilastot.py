4c7b644da0777c8cfc7358e4913a20e2ccaabb1a
hanezki <hanezki.manninen@gmail.com>
2021-10-25 11:35:30 +0300

    Harjoitus1_tilastot tehtävä valmis ja päivitetty .idea gitignoreen

--- Viikko3_Python_harjoituksia/Harjoitus1_tilastot/Harjoitus1_tilastot/Harjoitus1_tilastot.py
+++ Viikko3_Python_harjoituksia/Harjoitus1_tilastot/Harjoitus1_tilastot/Harjoitus1_tilastot.py
@@ -1,1 +1,21 @@
+import statistics
 
+# laske numerolistasta erilaisia tilastoja
+def näytä_tilastot(numerot):
+    minimi = min(numerot)
+    maksimi = max(numerot)
+    keskiarvo = statistics.mean(numerot)
+    mediaani = statistics.median(numerot)
+    moodi = statistics.mode(numerot)
+    print(f"Minimi: {minimi}\nmaksimi: {maksimi}\nkeskiarvo: {keskiarvo}\nmediaani: {mediaani}\nmoodi: {moodi}")
+
+try:
+    numerot = input("Anna lista numeroita, erota numerot pilkulla (esim. 1, 2, 3, 4)\n>")
+except ValueError:
+    print("Virheellinen syöte")
+
+# pilko lista "," merkin kohdilla ja muuta string lista numero listaksi
+numerot_lista = numerot.split(",")
+numerot_lista = [int(i) for i in numerot_lista]
+
+näytä_tilastot(numerot_lista)
