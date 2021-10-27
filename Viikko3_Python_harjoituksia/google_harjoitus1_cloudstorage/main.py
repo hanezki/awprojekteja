e199e48e038fe4a88b3222b02a11a08177a1dff5
hanezki <hanezki.manninen@gmail.com>
2021-10-27 10:14:06 +0300

    google_harjoitus1_cloudstorage osa 2, bucketin luominen valmis

--- Viikko3_Python_harjoituksia/google_harjoitus1_cloudstorage/main.py
+++ Viikko3_Python_harjoituksia/google_harjoitus1_cloudstorage/main.py
@@ -1,10 +1,19 @@
 from google.cloud import storage
 
+storage_client = storage.Client()
+
+#list buckets
 def list_buckets():
-    storage_client = storage.Client()
     buckets = storage_client.list_buckets()
 
     for bucket in buckets:
         print(bucket.name)
 
-list_buckets()
+def create_bucket(bucket_name):
+    bucket = storage_client.bucket(bucket_name)
+    bucket.storage_class = "COLDLINE"
+    new_bucket = storage_client.create_bucket(bucket, location="us")
+
+    print(f"Created bucket {new_bucket.name} in {new_bucket.location} with storage class {new_bucket.storage_class}")
+
+create_bucket("juukeli")
