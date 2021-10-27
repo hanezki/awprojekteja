10fa2fad440c5475de401ab436782dc9f3872d6c
hanezki <hanezki.manninen@gmail.com>
2021-10-27 10:44:36 +0300

    google_harjoitus1_cloudstoragen kohta 3, upload file to bucket valmis

--- Viikko3_Python_harjoituksia/google_harjoitus1_cloudstorage/main.py
+++ Viikko3_Python_harjoituksia/google_harjoitus1_cloudstorage/main.py
@@ -2,12 +2,14 @@ from google.cloud import storage
 
 storage_client = storage.Client()
 
+
 #list buckets
 def list_buckets():
     buckets = storage_client.list_buckets()
 
     for bucket in buckets:
         print(bucket.name)
+
 
 def create_bucket(bucket_name):
     bucket = storage_client.bucket(bucket_name)
@@ -16,4 +18,13 @@ def create_bucket(bucket_name):
 
     print(f"Created bucket {new_bucket.name} in {new_bucket.location} with storage class {new_bucket.storage_class}")
 
-create_bucket("juukeli")
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(source_file_name)
+
+    print(f"File {source_file_name} uploaded to {destination_blob_name}")
+
+
+upload_blob("juukeli", "teksti.txt", "teksti.txt")
