231c22dbd51ecf9de63323331542cc238b46e548
hanezki <hanezki.manninen@gmail.com>
2021-11-03 16:46:17 +0200

    tehtäviä tehty eteenpäin

---
+++ Viikko4_Python_harjoituksia/sql_python/src/scripts/read_image_into_file.py
@@ -0,0 +1,22 @@
+import psycopg2
+from config import config as config
+import PIL.Image as Image
+
+con = None
+try:
+    con = psycopg2.connect(**config())
+    cursor = con.cursor()
+
+    SQL = "SELECT file_data FROM files WHERE orig_filename = 'kitten.png';"
+    cursor.execute(SQL)
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+finally:
+    if con is not None:
+        con.close()
